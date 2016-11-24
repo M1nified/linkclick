708421910b6f06e4ac833a81d4875d6b593659ee
M!nified <gora.pub@gmail.com>
2016-11-24 11:49:19 +0100

    +add

--- settings_page.php
+++ settings_page.php
@@ -7,11 +7,28 @@
 
 <?php 
 include_once realpath(dirname(__FILE__).'/variables.php');
-// echo realpath(dirname(__FILE__).'/functions.php');
 // include_once realpath(dirname(__FILE__).'/functions.php');
+echo realpath(dirname(__FILE__).'/functions.php');
+include_once realpath(dirname(__FILE__).'/functions.php');
 
 global $wpdb;
 global $db_links;
+
+// ----- Add --------
+if(isset($_POST['mode']) && $_POST['mode'] === 'add'){
+    $wpdb->insert(
+        $wpdb->posts,
+        array(
+            'Ticket' => get_new_ticket(),
+            'CategoryId' => '',
+            'SubCategoryId' => '',
+            'Name' => '',
+            'JustTrack' => $_POST['JustTrack'],
+            'PostId' => $_POST['PostId']
+        )
+    );
+}
+
 
 // ----- Saving -----
 // print_r($_POST);
@@ -38,7 +55,14 @@ $limit_to = $limit_from + $limit_count;
 if($search_for == ''){
     $rows = $wpdb->get_results("SELECT * FROM {$db_links} ORDER BY `Target` LIMIT {$limit_from},{$limit_to}");
 }else{
-    $rows = $wpdb->get_results("SELECT * FROM {$db_links} WHERE `Target` LIKE '%{$search_for}%' OR `Name` LIKE '%{$search_for}%' ORDER BY `Target` LIMIT {$limit_from},{$limit_to}");
+    $rows = $wpdb->get_results("SELECT * FROM {$db_links} AS l
+    LEFT JOIN
+        {$wpdb->posts} AS posts ON posts.ID = l.PostId
+    WHERE
+        `Target` LIKE '%{$search_for}%'
+        OR `Name` LIKE '%{$search_for}%'
+    ORDER BY `Target`
+    LIMIT {$limit_from},{$limit_to}");
 }
 $limit_from_left = $limit_from-$limit_count-1;
 $limit_from_left = $limit_from_left < 0 ? 0 : $limit_from_left;
@@ -48,6 +72,35 @@ $base_url = preg_replace('/&?limit_from=\d*/','',$base_url);
 // print_r(parse_url($_SERVER['REQUEST_URI']));
 ?>
 <h1>LinkClick</h1>
+<h2>Add</h2>
+<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post" style="display: block;">
+    <input type="hidden" name="mode" value="add">
+    <label style="display:block;">Page: <input type="checkbox" name="is_post"></label>
+    <label style="display:block;">Url: <input type="text" name="url"></label>
+    <label style="display:block;">Post ID: <input type="text" name="PostId" class="lc-add-PostId"></label>
+    <label style="display:block;">Search post:
+        <select class="lc-add-list_posts">
+            <option value=""></option>
+            <?php
+                $posts = $wpdb->get_results("SELECT ID,post_title FROM {$wpdb->posts}");
+                foreach ($posts as $key => $row){
+                    print("<option value=\"{$row->ID}\">{$row->post_title}</option>");
+                }
+            ?>
+        </select>
+    </label>
+    <p>
+    <label> JustTrack: <input type="checkbox" name="JustTrack"></label>
+    </p>
+    <p><input type="submit" class="button"></p>
+    <script>
+        document.querySelector(".lc-add-list_posts").addEventListener('change',function(){
+            console.log(this.selectedOptions[0].value)
+            document.querySelector(".lc-add-PostId").value = this.selectedOptions[0].value;
+        });
+    </script>
+</form>
+<h2>Explore</h2>
 <form action="#" method="get" style="display:block; text-align: right;">
     <label>Search: <input type="text" name="search_for" value="<?php echo $search_for; ?>" style="width:20em;"></label>
     <label>Items per page: <input type="number" name="limit_count" value="<?php echo $limit_count; ?>" style="width:7em;"></label>
@@ -56,6 +109,7 @@ $base_url = preg_replace('/&?limit_from=\d*/','',$base_url);
     <input type="submit" class="button">
 </form>
 <p style="text-align: right;">
+<a href="<?php echo $base_url; ?>" class="button">|&lt;</a>
 <a href="<?php echo $base_url.'&limit_from='.$limit_from_left.'&limit_count='.$limit_count; ?>" class="button">&lt;&lt;</a>
 <a href="<?php echo $base_url.'&limit_from='.($limit_to+1).'&limit_count='.$limit_count; ?>" class="button">>></a>
 </p>
@@ -84,8 +138,8 @@ foreach ($rows as $key => $row) {
     // }
     echo "<tr>";
     echo "<td>{$row->Id}</td>";
-    echo "<td><input type=\"text\" value=\"".$row->Ticket."\" readonly disabled></td>";
-    echo "<td><input type=\"text\" value=\"{$row->Target}\" readonly disabled></td>";
+    echo "<td><input type=\"text\" value=\"".$row->Ticket."\" readonly></td>";
+    echo "<td><input type=\"text\" value=\"{$row->Target}\" readonly></td>";
     echo "<td>{$row->Name}</td>";
     echo "<td></td>";
     echo "<td></td>";
--- setup.php
+++ setup.php
@@ -16,10 +16,48 @@
 
  add_action('admin_menu','linkclick\add_menu_settings');
 
+add_action( 'init', 'linkclick\shall_lock' );
 function shall_lock() {
     // tutaj bedzie sprawdzanie czy zalogowany i czy powinien byc
     print_r("shalllock".is_user_logged_in());
+    $is_user_logged_in = is_user_logged_in(); 
+    return $is_user_logged_in;
 }
 
-add_action( 'init', 'linkclick\shall_lock' );
+add_filter('manage_post_posts_columns', 'linkclick\add_quick_edit_column');
+function add_quick_edit_column($columns) {
+    $columns['widget_set'] = 'LinkClick';
+    return $columns;
+}
 
+/*add_action('manage_posts_custom_column', 'linkclick\add_column_content', 10, 2);
+add_action('manage_pages_custom_column', 'linkclick\add_column_content', 10, 2);
+function add_column_content($column_name, $id) {
+    switch ($column_name) {
+    case 'widget_set':
+        echo 'ok';           
+        break;
+    }
+}
+
+add_action( 'quick_edit_custom_box', 'linkclick\add_quick_edit', 'page');
+function add_quick_edit($column_name, $post_type){
+    ?>
+    <fieldset class="inline-edit-col-right inline-edit-book" style="clear:both;">
+        <div class="inline-edit-col column-<?php echo $column_name; ?>">
+        <label class="inline-edit-group">
+        <?php 
+            switch ( $column_name ) {
+            case 'widget_set':
+                ?>
+                <span class="title">Author</span><input name="book_author" />
+                <?php
+                break;
+            }
+        ?>
+        </label>
+        </div>
+    </fieldset>
+    <?php
+}
+*/

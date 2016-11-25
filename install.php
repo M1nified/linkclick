e38be63ff81ff59e009ead5c3dfaa18853eef126
M!nified <gora.pub@gmail.com>
2016-11-25 14:28:09 +0100

    install db creation

--- install.php
+++ install.php
@@ -6,16 +6,41 @@ global $db_links;
 function create_tables(){
     global $wpdb;
     global $db_links;
-    $tabquery = "CREATE TABLE `{$db_links}` (
-    `Id` INT NOT NULL AUTO_INCREMENT,
-    `Ticket` NVARCHAR(256) NULL DEFAULT NULL,
-    `Target` NVARCHAR(1000) NULL DEFAULT NULL,
-    `CategoryId` INT NULL DEFAULT NULL,
-    `SubCategoryId` INT NULL DEFAULT NULL,
-    `Name` NVARCHAR(256) NULL DEFAULT NULL,
-    `JustTrack` int(11) DEFAULT '0',
-    PRIMARY KEY (`Id`),
-    UNIQUE INDEX `Id_UNIQUE` (`Id` ASC));";
+    global $lc_db_link;
+    global $lc_db_log;
+    global $lc_db_category;
+    $tabquery = "CREATE TABLE {$db_links} (
+        `Id` int(11) NOT NULL AUTO_INCREMENT,
+        `Ticket` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
+        `Target` varchar(1000) CHARACTER SET utf8 DEFAULT NULL,
+        `CategoryID` int(11) DEFAULT NULL,
+        `SubCategoryId` int(11) DEFAULT NULL,
+        `Name` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
+        `JustTrack` int(11) DEFAULT '0',
+        `PostId` bigint(20) DEFAULT NULL,
+        PRIMARY KEY (`Id`),
+        UNIQUE KEY `Id_UNIQUE` (`Id`),
+        UNIQUE KEY `post_id_UNIQUE` (`PostId`)
+        ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+        CREATE TABLE {$lc_db_category} (
+        `CategoryID` int(11) NOT NULL AUTO_INCREMENT,
+        `MasterCategoryID` int(11) DEFAULT NULL,
+        `Name` varchar(256) COLLATE utf8_bin DEFAULT NULL,
+        PRIMARY KEY (`CategoryID`),
+        UNIQUE KEY `CategoryID_UNIQUE` (`CategoryID`)
+        ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='			';
+
+        CREATE TABLE {$lc_db_log} (
+        `LogID` int(11) NOT NULL,
+        `LinkID` int(11) DEFAULT NULL,
+        `UserID` bigint(20) DEFAULT NULL,
+        `Date` datetime DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (`LogID`),
+        UNIQUE KEY `LogID_UNIQUE` (`LogID`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+    ";
     $wpdb->query($tabquery);
 }
 function install(){

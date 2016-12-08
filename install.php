c501b6a797ecb71f3369d73dd95da500f0c3b1e1
M!nified <gora.pub@gmail.com>
2016-12-08 13:06:01 +0100

    db fix

--- install.php
+++ install.php
@@ -9,7 +9,9 @@ function create_tables(){
     global $lc_db_link;
     global $lc_db_log;
     global $lc_db_category;
-    $tabquery = "CREATE TABLE {$db_links} (
+    $tabquery = "
+    
+        CREATE TABLE {$lc_db_link} (
         `Id` int(11) NOT NULL AUTO_INCREMENT,
         `Ticket` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
         `Target` varchar(1000) CHARACTER SET utf8 DEFAULT NULL,
@@ -37,7 +39,9 @@ function create_tables(){
         `UserID` bigint(20) DEFAULT NULL,
         `Date` datetime DEFAULT CURRENT_TIMESTAMP,
         PRIMARY KEY (`LogID`),
-        UNIQUE KEY `LogID_UNIQUE` (`LogID`)
+        UNIQUE KEY `LogID_UNIQUE` (`LogID`),
+        KEY `link_idx` (`LinkID`),
+        CONSTRAINT `link` FOREIGN KEY (`LinkID`) REFERENCES `{$lc_db_link}` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
     ";

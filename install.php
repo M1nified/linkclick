52148f8b60c57441ab63bfba3cda96f0e3a82ec5
M!nified <gora.pub@gmail.com>
2017-02-20 09:21:07 +0100

    install.php create table statements

--- install.php
+++ install.php
@@ -11,50 +11,46 @@ function create_tables(){
     global $lc_db_category;
     global $lc_db_settings;
     $tabquery = "
-    
-        CREATE TABLE {$lc_db_link} (
-        `Id` int(11) NOT NULL AUTO_INCREMENT,
-        `Ticket` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
-        `Target` varchar(1000) CHARACTER SET utf8 DEFAULT NULL,
-        `CategoryID` int(11) DEFAULT NULL,
-        `SubCategoryId` int(11) DEFAULT NULL,
-        `Name` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
-        `Secure` int(11) DEFAULT '0',
-        `PostId` bigint(20) DEFAULT NULL,
-        PRIMARY KEY (`Id`),
-        UNIQUE KEY `Id_UNIQUE` (`Id`),
-        UNIQUE KEY `post_id_UNIQUE` (`PostId`)
-        ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+        CREATE TABLE IF NOT EXISTS `{$lc_db_category}` (
+            `CategoryID` int(11) NOT NULL AUTO_INCREMENT,
+            `MasterCategoryID` int(11) DEFAULT NULL,
+            `Name` varchar(256) COLLATE utf8_bin DEFAULT NULL,
+            `IsValid` bit(1) DEFAULT b'1',
+            PRIMARY KEY (`CategoryID`),
+            KEY `CategoryID_UNIQUE` (`CategoryID`)
+        ) ENGINE=InnoDB AUTO_INCREMENT=104 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='			';
 
-        CREATE TABLE {$lc_db_category} (
-        `CategoryID` int(11) NOT NULL AUTO_INCREMENT,
-        `MasterCategoryID` int(11) DEFAULT NULL,
-        `Name` varchar(256) COLLATE utf8_bin DEFAULT NULL,
-        PRIMARY KEY (`CategoryID`),
-        UNIQUE KEY `CategoryID_UNIQUE` (`CategoryID`)
-        ) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='			';
-
-        CREATE TABLE {$lc_db_log} (
-        `LogID` int(11) NOT NULL,
-        `LinkID` int(11) DEFAULT NULL,
-        `UserID` bigint(20) DEFAULT NULL,
-        `Date` datetime DEFAULT CURRENT_TIMESTAMP,
-        PRIMARY KEY (`LogID`),
-        UNIQUE KEY `LogID_UNIQUE` (`LogID`),
-        KEY `link_idx` (`LinkID`),
-        CONSTRAINT `link` FOREIGN KEY (`LinkID`) REFERENCES `{$lc_db_link}` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+        CREATE TABLE IF NOT EXISTS `{$lc_db_link}` (
+            `Id` int(11) NOT NULL AUTO_INCREMENT,
+            `Ticket` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
+            `Target` varchar(1000) CHARACTER SET utf8 DEFAULT NULL,
+            `CategoryID` int(11) DEFAULT NULL,
+            `SubCategoryId` int(11) DEFAULT NULL,
+            `Name` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
+            `Secure` int(11) DEFAULT '0',
+            `PostId` bigint(20) DEFAULT NULL,
+            PRIMARY KEY (`Id`),
+            UNIQUE KEY `Id_UNIQUE` (`Id`),
+            UNIQUE KEY `post_id_UNIQUE` (`PostId`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-        CREATE TABLE `{$lc_db_settings}` (
-        `id` int(11) NOT NULL AUTO_INCREMENT,
-        `option_name` varchar(250) COLLATE utf8_bin DEFAULT NULL,
-        `option_value` varchar(250) CHARACTER SET utf8 DEFAULT NULL,
-        `option_reference` int(11) NOT NULL,
-        PRIMARY KEY (`id`),
-        UNIQUE KEY `id_UNIQUE` (`id`)
+        CREATE TABLE IF NOT EXISTS `{$lc_db_log}` (
+            `log_id` int(11) NOT NULL AUTO_INCREMENT,
+            `post_id` int(11) DEFAULT NULL,
+            `user_id` bigint(20) DEFAULT NULL,
+            `log_date` datetime NOT NULL,
+            PRIMARY KEY (`log_id`),
+            UNIQUE KEY `LogID_UNIQUE` (`log_id`)
+        ) ENGINE=InnoDB AUTO_INCREMENT=418 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+        CREATE TABLE IF NOT EXISTS `{$lc_db_settings}` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `option_name` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+            `option_value` varchar(250) CHARACTER SET utf8 DEFAULT NULL,
+            `option_reference` int(11) NOT NULL,
+            PRIMARY KEY (`id`),
+            UNIQUE KEY `id_UNIQUE` (`id`)
         ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-
     ";
     $wpdb->query($tabquery);
 }
